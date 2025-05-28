dadfd64f19fcab03f9b46ac3e0e0bcb46efaf76e
Dongximing <dongx34@myumanitoba.ca>
2025-05-28 15:00:19 -0400

    for wail

--- generate_samples.py
+++ generate_samples.py
@@ -173,7 +173,7 @@ def process_file_to_pickle(json_path, out_pkl_path, tokenizer, model, num_genera
 
 
 
-def inference_model_pickle(task_name: str, model, tokenizer, base_dir='/home/shaowei/hf/math-result_left',
+def inference_model_pickle(task_name: str, model, tokenizer, base_dir='/home/cs/staff/shaowei/hf/math-result_left',
                            start=0, end=250, num_generations=20):
 
     for number in range(start, end):
