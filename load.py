bd60a2681785eeb80bf5edae65bba2455481e89e
Dongximing <dongx34@myumanitoba.ca>
2025-05-29 11:50:54 -0400

    add the data

--- load.py
+++ load.py
@@ -4,6 +4,8 @@ import openai
 import os
 openai.api_key =os.environ["OPENAI_API_KEY"]
 from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import sys
 def get_openai_embeddings(texts, model="text-embedding-3-small", batch_size=20):
     """
     texts: list of strings,
@@ -21,6 +23,15 @@ def get_openai_embeddings(texts, model="text-embedding-3-small", batch_size=20):
     return np.array(embeddings)
 
 with open("/home/shaowei/hf/math-result_left/data-500-temp0_10/generations_10.pkl", "rb") as f:
+    tokenizer = AutoTokenizer.from_pretrained(
+        "Qwen/QwQ-32B-AWQ",
+        trust_remote_code=True
+    )
     generations = pickle.load(f)
-    print(generations[1])
+    for g in generations:
+        print(tokenizer.encode(g['input_text']))
+        print("------------------------")
+        print(tokenizer.encode(g['predicted_answer']))
+        sys.exit()
+
 
