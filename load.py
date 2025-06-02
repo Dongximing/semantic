43cc22a32b91e15acd22d35a222dbc285b58584b
Dongximing <dongx34@myumanitoba.ca>
2025-06-02 13:00:58 -0400

    add the quail path

--- load.py
+++ load.py
@@ -256,7 +256,7 @@ def inference_model_pickle(task_name: str = None, model=None, tokenizer=None,
         dirname = f'data-500-temp0_{number}'
         dir_path = os.path.join(base_dir, dirname)
         json_path = os.path.join(dir_path, f'new_generations_{number}.pkl')
-        out_pkl_path = os.path.join(dir_path, f'label_semantic_{number}.pkl')
+        out_pkl_path = os.path.join(dir_path, f'test_{number}.pkl')
         if not os.path.exists(json_path):
             logger.warning(f"{json_path} does not exist, skipping.")
             continue
