2a7e5d0a12fdafd53a136b17f171f361198ba7b3
Dongximing <dongx34@myumanitoba.ca>
2025-06-03 16:51:03 -0400

    add semantic entrpy

--- checking.py
+++ checking.py
@@ -1,6 +1,6 @@
 import pickle
 
-with open('/data/ximing/math-result_left/data-500-temp0_0', "rb") as f:
+with open('/data/ximing/math-result_left/data-500-temp0_0/ww0.pkl', "rb") as f:
     generations = pickle.load(f)
     for g in generations:
         if 'cluster_assignment_entropy' in g:
