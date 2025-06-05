214630a9024d2abd3d7d1d285c273cca7541aa7c
Ximing Dong <ximing@mac.home>
2025-06-04 22:05:58 -0400

    a

--- checking.py
+++ checking.py
@@ -7,4 +7,4 @@ with open('/data/ximing/math-result_left/data-500-temp0_0/ww0.pkl', "rb") as f:
             print('cluster_assignment_entropy',g['cluster_assignment_entropy_deberta'])
         elif 'clustering-gpt-prompty_deberta' in g:
             print('clustering-gpt-prompty_deberta',g['clustering-gpt-prompty_deberta'])
-            print("aaaa")
+            ##
