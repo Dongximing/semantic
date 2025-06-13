b57251a5b1ca27b826955adcce68ad43ae49c16c
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-13 13:02:16 -0400

    doing exp

--- speculative/speculative_hf_decoding.py
+++ speculative/speculative_hf_decoding.py
@@ -153,7 +153,7 @@ def speculative_decoding(target_model, target_tokenizer, speculative_model,specu
         # since we first feed the input to the target model, we need to add record the length of the input text in the target model and speculative model both;
         # this one is for the edge case, We need to check that the target model generates the answer within 200 words, then we need to stop directly.
         start_target_model_inputs = target_tokenizer(target_text, return_tensors="pt").to(target_model.device)
-        target_prompt_len = start_target_model_inputs.shape[1]
+        target_prompt_len = start_target_model_inputs["input_ids"].shape[1]
         # original_speculative_text_len = len(speculative_text.shape[1])
         original_target_text_len = len(start_target_model_inputs.shape[1])
         # there are kv caches in both the target model and speculative model.
