98a35aa4013261a2d805fd24c661f3a53f0251e5
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-17 10:23:26 -0400

    doing exp

--- speculative/speculative_hf_decoding.py
+++ speculative/speculative_hf_decoding.py
@@ -216,7 +216,7 @@ def speculative_decoding(target_model, target_tokenizer, speculative_model,specu
         original_target_text_len = start_target_model_inputs["input_ids"].shape[1]
         # there are kv caches in both the target model and speculative model.
         spec_kv, tgt_kv = None, None
-        correct_tokens, try_correct_num = [], 0
+        correct_tokens, try_correct_num, correct_spe_number = [], 0, 0
         # change flag is used to check whether the target models need to guide the speculative model.
         token_num, change_tokens, change_flag = 0, 0, False
         # 'begin' implicate the first time we start the speculative model.
@@ -297,6 +297,7 @@ def speculative_decoding(target_model, target_tokenizer, speculative_model,specu
                 # if the prob of the target model is lower than the prob of the speculative model, we use the target model to generate the current part.
                 print(f'prob_target: {prob_target}, prob_spec:{prob_spec} ')
                 if prob_target.item() >= prob_spec.item():
+                    correct_spe_number +=1
                     use_target = False
                     valid_tgt_kv = copy.deepcopy(checking_tgt_kv)# we just want to real generation KV cache,
                     spec_kv = copy.deepcopy(checking_spec_kv)
@@ -354,7 +355,7 @@ def speculative_decoding(target_model, target_tokenizer, speculative_model,specu
 
         print('-------------------------------------end the process -------------------------\n\n\n')
         print(generated_text)
-        return generated_text, try_correct_num
+        return generated_text, try_correct_num,correct_spe_number
 
 
 
@@ -366,14 +367,16 @@ def process_file_to_json(dir_path, target_model, target_tokenizer,speculative_mo
     start_time = time.time()
     result = speculative_decoding(target_model, target_tokenizer, speculative_model, speculative_tokenizer, problem, target_temperature, speculative_temperature,max_new_tokens,model_target_probe,model_spec_probe)
     end_time = time.time()
-    generated_text, try_correct_num = result
+    generated_text, try_correct_num,correct_spe_number = result
 
     all_generations.append({
         "input_text": problem,
         "real_answer": generated_text,
         "try_correct_num": try_correct_num,
         "standard_answer": answer,
-        "execution_time": f"{end_time - start_time:.2f}s"
+        "execution_time": f"{end_time - start_time:.2f}s",
+        "correct_spe_number":correct_spe_number
+
     })
     # except Exception as e:
     #     all_generations.append({
@@ -393,17 +396,17 @@ def process_file_to_json(dir_path, target_model, target_tokenizer,speculative_mo
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str,  help="dataset",default='math-500')
+    parser.add_argument("--dataset", type=str,  help="dataset",default='aime')
     parser.add_argument("--target_model", type=str,  help="target_model",default="Qwen/QwQ-32B-AWQ")
     parser.add_argument("--speculative_model", type=str,  help="speculative_model", default="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
-    parser.add_argument("--data_dir", type=str,  help="data_dir",default='/data/semantic/speculative/spec_reasult_math-500')
+    parser.add_argument("--data_dir", type=str,  help="data_dir",default='/data/semantic/speculative/spec_reasult_aime')
     parser.add_argument("--start_dataset", type=int, help="the beginning of the dataset",default=0)
-    parser.add_argument("--end_dataset", type=int, help="the end of the dataset",default=1)
-    parser.add_argument("--target_probe", type=str, help="target_probe",default="/data/semantic/training/math-500_output_last_hidden_list_best_probe_mse")
-    parser.add_argument("--speculative_probe", type=str, help="speculative_probe",default="/home/shaowei/training_probe/math-500_output_last_hidden_list_best_probe_mse")
+    parser.add_argument("--end_dataset", type=int, help="the end of the dataset",default=30)
+    parser.add_argument("--target_probe", type=str, help="target_probe",default="/data/semantic/training/aime_output_last_hidden_list_best_probe_mse")
+    parser.add_argument("--speculative_probe", type=str, help="speculative_probe",default="/home/shaowei/training_probe/aime_output_last_hidden_list_best_probe_mse")
     parser.add_argument("--target_temperature", type=float, help="target_temperature",default=0.1)
     parser.add_argument("--speculative_temperature", type=float, help="speculative_temperature",default=0.6)
-    parser.add_argument("--max_new_tokens", type=int, help="max_new_tokens",default=12000)
+    parser.add_argument("--max_new_tokens", type=int, help="max_new_tokens",default=14000)
     parser.add_argument("--top_p", type=float, help="top_p",default=0.9)
     parser.add_argument("--top_k", type=int, help="top_k",default=50)
     args = parser.parse_args()
