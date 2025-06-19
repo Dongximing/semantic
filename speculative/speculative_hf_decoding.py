692753663e2754024a3df33dd94f2a56fe264a84
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-19 13:39:45 -0400

    doing eval

--- speculative/speculative_hf_decoding.py
+++ speculative/speculative_hf_decoding.py
@@ -407,8 +407,8 @@ if __name__ == "__main__":
     parser.add_argument("--target_model", type=str,  help="target_model",default="Qwen/QwQ-32B-AWQ")
     parser.add_argument("--speculative_model", type=str,  help="speculative_model", default="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
     parser.add_argument("--data_dir", type=str,  help="data_dir",default='/data/semantic/speculative/spec_result_math-500_seed_')
-    parser.add_argument("--start_dataset", type=int, help="the beginning of the dataset",default=2)
-    parser.add_argument("--end_dataset", type=int, help="the end of the dataset",default=30)
+    parser.add_argument("--start_dataset", type=int, help="the beginning of the dataset",default=100)
+    parser.add_argument("--end_dataset", type=int, help="the end of the dataset",default=400)
     parser.add_argument("--target_probe", type=str, help="target_probe",default="/data/semantic/training/math-500_output_last_hidden_list_best_probe_mse")#aime_output_last_hidden_list_best_probe_mse
     parser.add_argument("--speculative_probe", type=str, help="speculative_probe",default="/home/shaowei/training_probe/math-500_output_last_hidden_list_best_probe_mse")
     parser.add_argument("--target_temperature", type=float, help="target_temperature",default=0.1)
