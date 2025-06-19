52593cfca3784761fe07debf18e4c48b7d5036f4
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-19 14:17:51 -0400

    doing eval

--- speculative/eval.py
+++ speculative/eval.py
@@ -15,7 +15,7 @@ def check_math_correctness(ref, generation):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--start', type=int, default=100)
-    parser.add_argument('--end', type=int, default=300)
+    parser.add_argument('--end', type=int, default=180)
     parser.add_argument('--dataset', type=str, default='math-500')
     parser.add_argument('--eval_path', type=str, default='/data/semantic/speculative/spec_result_math-500_seed_42')
     #/home/cs/staff/shaowei/semantic
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     seed_everything(args.seed)
 
     number_correct = 0
+    number_of_tokens = 0
     total_number = args.end - args.start
 
     for idx, number in enumerate(tqdm(range(args.start, args.end))):
@@ -43,12 +44,14 @@ if __name__ == '__main__':
             predict = generations[0]['real_answer']
             #print(predict)
             standard = generations[0]['standard_answer']
+            number_of_tokens += generations[0]['length_of_real_output']
         result = check_math_correctness(standard,predict)
         if result:
             number_correct += 1
         else:
             print(f'Error in {dirname}')
     print(f'Accuracy: {number_correct / total_number} in {args.dataset}')
+    print("Number of tokens: ", number_of_tokens/total_number)
     print(f'Number_correct: {number_correct}')
     print(f'Total: {total_number}')
 
