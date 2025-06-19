ca372689fba19cd93f6a922d1ad049f5076e2e42
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-19 14:30:58 -0400

    doing eval

--- baseline/baseline_eval.py
+++ baseline/baseline_eval.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     for idx, number in enumerate(tqdm(range(args.start, args.end))):
 
         if args.dataset == 'math-500':
-            dirname = f'spec_{args.dataset}_{number}'
+            dirname = f'seed_42_baseline_{args.dataset}_{number}'
         elif args.dataset == 'aime':
             dirname = f'spec_{args.dataset}_{number}'
         dir_path = os.path.join(args.eval_path, dirname)
