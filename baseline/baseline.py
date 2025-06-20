b964edfc3ba5ec5815e5a46c75dc2444988cc34d
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-06-20 12:40:11 -0400

    doing eval

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -109,7 +109,8 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, help="dataset", default='aime')  # math-500
     parser.add_argument("--seed", type=int, help="seed", default=123)
     parser.add_argument("--model", type=str, help="model", default="Qwen/QwQ-32B-AWQ")
-    parser.add_argument("--start", type=int, help="model", default=4)
+    parser.add_argument("--start", type=int, help="start", default=4)
+    parser.add_argument("--end", type=int, help="end", default=30)
     args = parser.parse_args()
     seed_everything(args.seed)
     tokenizer = AutoTokenizer.from_pretrained(
@@ -129,7 +130,7 @@ if __name__ == "__main__":
         tokenizer=tokenizer,
         base_dir=base_dir,
         start=args.start,
-        end=30,
+        end=args.end,
         seed=args.seed
     )
     print("done")
