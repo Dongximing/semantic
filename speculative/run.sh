4923ea9bd0e85866db9c7b6a90444a4058acc77d
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-08 13:19:22 -0400

    add

--- speculative/run.sh
+++ speculative/run.sh
@@ -4,6 +4,6 @@
 #python speculative_hf_decoding.py --seed 98  --dataset aime --start_dataset 0 --end_dataset 30
 #python speculative_hf_decoding.py --seed 498  --dataset aime --start_dataset 0 --end_dataset 30
 #python speculative_hf_decoding.py --seed 398  --dataset aime --start_dataset 0 --end_dataset 30
-python speculative_hf_decoding.py --seed 98  --dataset math-500 --start_dataset 324 --end_dataset 500
+python speculative_hf_decoding.py --seed 98  --dataset math-500 --start_dataset 341 --end_dataset 500
 python speculative_hf_decoding.py --seed 198  --dataset math-500 --start_dataset 100 --end_dataset 500
 python speculative_hf_decoding.py --seed 298  --dataset math-500 --start_dataset 100 --end_dataset 500
