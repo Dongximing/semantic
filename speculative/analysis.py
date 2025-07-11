86b4acfc284403a4dd247758314ab783255d281d
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-11 10:44:21 -0400

    add

--- speculative/analysis.py
+++ speculative/analysis.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='math-500')
     parser.add_argument('--eval_path', type=str, default='/data/semantic/speculative/spec_result_math-500_seed_456')
     args = parser.parse_args()
+    length_target = 0
+    length_spe = 0
+    count = 0
     tokenizer = AutoTokenizer.from_pretrained('unsloth/DeepSeek-R1-Distill-Qwen-32B-bnb-4bit')
     for idx, number in enumerate(tqdm(range(args.start, args.end))):
 
@@ -23,9 +26,7 @@ if __name__ == '__main__':
         if not os.path.exists(json_path):
             print(f"[Warning] {json_path} does not exist, skipping...")
             continue
-        length_target = 0
-        length_spe = 0
-        count = 0
+
 
         with open(json_path, "r", encoding="utf-8") as f:
             generations = json.load(f)
@@ -36,8 +37,8 @@ if __name__ == '__main__':
                     length_target += tokenizer(d['target_model'], return_tensors="pt")['input_ids'].shape[1]
                     length_spe += tokenizer(d['why_is_not_good'], return_tensors="pt")['input_ids'].shape[1]
                     count += 1
-        print('length_target',length_target/count)
-        print('length_spe',length_spe/count)
+    print('length_target',length_target/count)
+    print('length_spe',length_spe/count)
 
 
 
