bb0b011f194a22c491a365ea040f84deb3f6f812
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-14 15:59:44 -0400

    add

--- labeling.py
+++ labeling.py
@@ -343,9 +343,9 @@ def inference_model_pickle(task_name: str = None, model=None, tokenizer=None,
             continue
         dirname = f'data-60-temp0_{number}'
         dir_path = os.path.join(base_dir, dirname)
-        json_path = os.path.join(dir_path, f'new_generations_{number}.pkl')
+        json_path = os.path.join(dir_path, f'new_generations_with_entropy{number}.pkl')
 
-        out_pkl_path = os.path.join(dir_path, f'new_generations_with_entropy{number}.pkl')
+        out_pkl_path = os.path.join(dir_path, f'new_generations_with_entropy_prob{number}.pkl')
         if not os.path.exists(json_path):
             logger.warning(f"{json_path} does not exist, skipping.")
             continue
