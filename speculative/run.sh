981fb285e1f2fdf795ee496a11bc3155baff4ece
Ximing Dong <ximing@mac.home>
2025-07-14 21:07:28 -0400

    add

--- speculative/run.sh
+++ speculative/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET_PID=874681
+TARGET_PID=2917287
 CHECK_INTERVAL=10  # 每10秒检查一次
 
 echo "Waiting for process $TARGET_PID to finish..."
@@ -17,5 +17,5 @@ echo "Process $TARGET_PID has ended. Running your script..."
 #python speculative_hf_decoding.py --seed 98  --dataset math-500 --start_dataset 353 --end_dataset 500
 
 
-python speculative_hf_decoding.py --seed 298  --dataset math-500 --start_dataset 100 --end_dataset 240
-#python speculative_hf_decoding.py --seed 198  --dataset math-500 --start_dataset 100 --end_dataset 500
+python speculative_hf_decoding.py --seed 98  --dataset math-500 --start_dataset 0 --end_dataset 30
+python speculative_hf_decoding.py --seed 198  --dataset math-500 --start_dataset 0 --end_dataset 30
