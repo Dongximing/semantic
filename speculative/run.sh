766c677bc71da82d0d57fc07f6ac156449c12799
Ximing Dong <ximing@ae:f0:5b:4c:74:36.home>
2025-07-19 10:27:41 -0400

    add

--- speculative/run.sh
+++ speculative/run.sh
@@ -11,13 +11,12 @@
 #done
 #
 #echo "Process $TARGET_PID has ended. Running your script..."
-python speculative_hf_decoding.py --seed 98   --dataset aime --start_dataset 4 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
-python speculative_hf_decoding.py --seed 2098   --dataset aime --start_dataset 0 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
-python speculative_hf_decoding.py --seed 3098   --dataset aime --start_dataset 0 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
-#python speculative_hf_decoding.py --seed 98   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_  --target_probe /data/semantic/training/new_deepseekr132b_math-500_output_last_hidden_list_best_probe_mse
-
-#python speculative_hf_decoding.py --seed 2023   --dataset aime --start_dataset 0 --end_dataset 30 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
-#python speculative_hf_decoding.py --seed 2023   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_  --target_probe /data/semantic/training/new_deepseekr132b_math-500_output_last_hidden_list_best_probe_mse
+#python speculative_hf_decoding.py --seed 98   --dataset aime --start_dataset 4 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
+#python speculative_hf_decoding.py --seed 2098   --dataset aime --start_dataset 0 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
+#python speculative_hf_decoding.py --seed 3098   --dataset aime --start_dataset 0 --end_dataset 29 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
+python speculative_hf_decoding.py --seed 2022   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_
+python speculative_hf_decoding.py --seed 2023   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_
+#python speculative_hf_decoding.py --seed 2024   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_  --target_probe /data/semantic/training/new_deepseekr132b_math-500_output_last_hidden_list_best_probe_mse
 
 #python speculative_hf_decoding.py --seed 2004   --dataset aime --start_dataset 0 --end_dataset 30 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
 #python speculative_hf_decoding.py --seed 2004   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_  --target_probe /data/semantic/training/new_deepseekr132b_math-500_output_last_hidden_list_best_probe_mse
