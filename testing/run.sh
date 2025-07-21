b9a859cf88e82eb14f60781b2bce35016a8adf4b
Ximing Dong <ximing@mac.home>
2025-07-21 18:51:35 -0400

    add

--- testing/run.sh
+++ testing/run.sh
@@ -12,13 +12,8 @@
 
 python test.py --seed 123 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
 python test.py --seed 456 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-python test.py --seed 789 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-python test.py --seed 43 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-python test.py --seed 42 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+
 
 
 python test.py --seed 123 --model unsloth/DeepSeek-R1-Distill-Qwen-1.5B-unsloth-bnb-4bit
 python test.py --seed 456 --model unsloth/DeepSeek-R1-Distill-Qwen-1.5B-unsloth-bnb-4bit
-python test.py --seed 789 --model unsloth/DeepSeek-R1-Distill-Qwen-1.5B-unsloth-bnb-4bit
-python test.py --seed 43 --model unsloth/DeepSeek-R1-Distill-Qwen-1.5B-unsloth-bnb-4bit
-python test.py --seed 42 --model unsloth/DeepSeek-R1-Distill-Qwen-1.5B-unsloth-bnb-4bit
