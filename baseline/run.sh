a7aff128d2fb4c2b5026161f53dd67d87ca48744
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-22 17:03:19 -0400

    add

--- baseline/run.sh
+++ baseline/run.sh
@@ -3,8 +3,8 @@
 python baseline.py --seed 2014 --start 0 --end 30 --model Qwen/QwQ-32B
 python baseline.py --seed 2014 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
 
-python baseline.py --seed 2015 --start 0 --end 30 --model Qwen/QwQ-32B
-python baseline.py --seed 2015 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
-
-python baseline.py --seed 2016 --start 0 --end 30 --model Qwen/QwQ-32B
-python baseline.py --seed 2016 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
+#python baseline.py --seed 2015 --start 0 --end 30 --model Qwen/QwQ-32B
+#python baseline.py --seed 2015 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
+#
+#python baseline.py --seed 2016 --start 0 --end 30 --model Qwen/QwQ-32B
+#python baseline.py --seed 2016 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
