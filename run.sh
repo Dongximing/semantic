29bd2540a87c08915582362140834f5b3c1eb415
Ximing Dong <ximing@ae:f0:5b:4c:74:36.home>
2025-07-26 22:07:12 -0400

    use qwq generate segment add wait and  wait

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET_PID=1009151
+CHECK_INTERVAL=10  # 每10秒检查一次
+echo "Waiting for process $TARGET_PID to finish..."
+
+# 循环检查进程是否存在
+while kill -0 $TARGET_PID 2>/dev/null; do
+    sleep $CHECK_INTERVAL
+done
+
+echo "Process $TARGET_PID has ended. Running your script..."
+python labeling.py --start 0 --end 58
