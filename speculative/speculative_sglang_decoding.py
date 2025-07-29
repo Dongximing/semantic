35f19a15cdcd18dbad68eb463910ec6ee0aa0e6c
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-29 11:02:55 -0400

    use qwq generate segment add wait and  wait

--- speculative/speculative_sglang_decoding.py
+++ speculative/speculative_sglang_decoding.py
@@ -339,7 +339,7 @@ if __name__ == "__main__":
         model_path=args.target_model,
         tp_size=4,
         enable_return_hidden_states=True,
-        mem_fraction_static=0.65
+        mem_fraction_static=0.75
     )
     target_tokenizer = transformers.AutoTokenizer.from_pretrained(
     args.target_model,
