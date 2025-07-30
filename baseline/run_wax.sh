d9c9c1177bff481916f5b2da08b3564103041061
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-30 15:10:24 -0400

    use qwq generate segment add wait and  wait

---
+++ baseline/run_wax.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python sgl_baseline.py --seed 123 --start 100 --end 500 --model DeepSeek-R1-Distill-1.5b
+python sgl_baseline.py --seed 456 --start 100 --end 500 --model DeepSeek-R1-Distill-1.5b
+python sgl_baseline.py --seed 789 --start 100 --end 500 --model DeepSeek-R1-Distill-1.5b
