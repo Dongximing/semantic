c6b9f1c05d0d9f8378d71f36faa45e6f06ba8a03
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-30 14:34:31 -0400

    use qwq generate segment add wait and  wait

--- baseline/run.sh
+++ baseline/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-python baseline.py --seed 2014 --start 0 --end 30 --model Qwen/QwQ-32B
-python baseline.py --seed 2014 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
+python sgl_baseline.py --seed 123 --start 14 --end 30 --model Qwen/QwQ-32B
+python sgl_baseline.py --seed 456 --start 3 --end 30 --model Qwen/QwQ-32B
+python sgl_baseline.py --seed 789 --start 3 --end 30 --model Qwen/QwQ-32B
 
 #python baseline.py --seed 2015 --start 0 --end 30 --model Qwen/QwQ-32B
 #python baseline.py --seed 2015 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
