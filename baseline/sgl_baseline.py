8dfb529169cd4fe0016fd17330595abba71be197
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-30 10:47:13 -0400

    use qwq generate segment add wait and  wait

--- baseline/sgl_baseline.py
+++ baseline/sgl_baseline.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
 
 
 
-    base_dir = f'/data/semantic/baseline/sgl_{model_name}_{args.dataset}_seed{args.seed}/'
+    base_dir = f'/home/cs/staff/shaowei/semantic/baseline/sgl_{model_name}_{args.dataset}_seed{args.seed}/'
     inference_model_pickle(
         task_name=args.dataset,
         base_dir=base_dir,
