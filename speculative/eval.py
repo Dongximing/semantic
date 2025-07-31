45d66abf8083beca11fe91f1afbf78b2b6458eec
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-31 09:52:55 -0400

    use qwq generate segment add wait and  wait

--- speculative/eval.py
+++ speculative/eval.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
             print(f'Error in {dirname}')
     print(f"total time: {time}")
     print(f'Accuracy: {number_correct / total_number} in {args.dataset}')
-    print("Number of tokens: ", number_of_tokens/number_correct)
+    print("Number of tokens: ", number_of_tokens/total_number)
     print("average spe step: ", spe_step/(spe_step+target_step))
     print("average target step: ", target_step / (spe_step + target_step))
     print("average execution time: ", time/total_number)
