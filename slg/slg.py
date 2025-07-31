712640ed176ffe00a69ae1aed9948c2056da00b7
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-07-31 14:55:53 -0400

    use qwq generate segment add wait and  wait

--- slg/slg.py
+++ slg/slg.py
@@ -1,12 +1,22 @@
 import requests
 
-url = "http://194.68.245.149:22096/generate"
-
-data = {
-    "text": "Hello",
-    "max_new_tokens": 100,
-    "temperature": 0.7
+url = "http://130.179.30.7:{30000}/generate"
+small_input  = 'hell0'
+sampling_params = {
+    "temperature": 0.6,
+    "top_p": 0.95,
+    "max_new_tokens": 500,
+    "stop_token_ids": [4710, 382, 1447, 271, 692, 1939, 2533, 3593],
+    "no_stop_trim": True
 }
+json_data = {
+    "text": [small_input],
+    "sampling_params": sampling_params,
+    "return_hidden_states": True,
+}
+speculative_outputs = requests.post(
+    f"http://130.179.30.7:{30000}/generate",
+    json=json_data,
+)
 
-response = requests.post(url, json=data)
-print(response.json())
+print(speculative_outputs.json())
