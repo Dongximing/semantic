c3125c44b15c3710cd9365d0e1051eb3c4d8c484
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-08-01 13:41:23 -0400

    add

--- slg/slg.py
+++ slg/slg.py
@@ -1,10 +1,10 @@
 import requests
 
-small_input  = 'hell0'
+small_input  = 'How many positive whole-number divisors does 196 have?'
 sampling_params = {
     "temperature": 0.6,
     "top_p": 0.95,
-    "max_new_tokens": 5,
+    "max_new_tokens":  1,
     "stop_token_ids": [4710, 382, 1447, 271, 692, 1939, 2533, 3593],
     "no_stop_trim": True
 }
@@ -13,8 +13,9 @@ json_data = {
     "sampling_params": sampling_params,
 }
 speculative_outputs = requests.post(
-    "https://rdd8sasuuy0y8l-8888.proxy.runpod.net/generate",
+    "https://m2d1q0gzo61thi-8800.proxy.runpod.net/generate",
     json=json_data,
+
 )
 
-print(speculative_outputs.json())
+print(speculative_outputs)
