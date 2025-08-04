c3737b0c62e43956fcb8452f69dffc534038795a
Ximing Dong <ximing@ximingdembp.home>
2025-08-03 21:39:02 -0400

    add

--- labeling.py
+++ labeling.py
@@ -160,9 +160,9 @@ def get_semantic_ids(strings_list, model,prefix, strict_entailment=True, tokeniz
     return semantic_set_ids
 def process_file_to_pickle(json_path, out_pkl_path):
 
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v2-xlarge-mnli")
-    model = AutoModelForSequenceClassification.from_pretrained(
-        "microsoft/deberta-v2-xlarge-mnli").to("cuda:2")
+    # tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v2-xlarge-mnli")
+    # model = AutoModelForSequenceClassification.from_pretrained(
+    #     "microsoft/deberta-v2-xlarge-mnli").to("cuda:2")
 
     group_size = 21
     with open(json_path, "rb") as f:
