3aed2923c1b80f294bd2cbd19b4c89ff27d0158c
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-08-05 15:23:47 -0400

    add

--- baseline/run.sh
+++ baseline/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+TARGET_PID=3854517
+CHECK_INTERVAL=10  # 每10秒检查一次
+
+echo "Waiting for process $TARGET_PID to finish..."
+
+# 循环检查进程是否存在
+while kill -0 $TARGET_PID 2>/dev/null; do
+    sleep $CHECK_INTERVAL
+done
+
+echo "Process $TARGET_PID has ended. Running your script..."
 python sgl_baseline.py --seed 123 --start 100 --end 500 --model Qwen/QwQ-32B
 python sgl_baseline.py --seed 456 --start 100 --end 500 --model Qwen/QwQ-32B
 python sgl_baseline.py --seed 789 --start 100 --end 500 --model Qwen/QwQ-32B
--- baseline/sgl_baseline.py
+++ baseline/sgl_baseline.py
@@ -122,7 +122,7 @@ def inference_model_pickle(task_name: str, tokenizer, base_dir,model,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, help="dataset", default='aime')  # math-500
+    parser.add_argument("--dataset", type=str, help="dataset", default='math-500')  # math-500
     parser.add_argument("--seed", type=int, help="seed", default=123)
     parser.add_argument("--model", type=str, help="model", default="Qwen/QwQ-32B")
     parser.add_argument("--start", type=int, help="start", default=0)
@@ -144,7 +144,7 @@ if __name__ == "__main__":
 
 
 
-    base_dir = f'/home/cs/staff/shaowei/semantic/baseline/sgl_{model_name}_{args.dataset}_seed{args.seed}/'
+    base_dir = f'/data/semantic/baseline/sgl_{model_name}_{args.dataset}_seed{args.seed}/'
     inference_model_pickle(
         task_name=args.dataset,
         base_dir=base_dir,
