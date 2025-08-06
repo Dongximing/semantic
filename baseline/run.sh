d108f92665ccdcd535afe0ea7802446798d1cc00
Ximing Dong <ximing@ae:f0:5b:4c:74:36.home>
2025-08-06 17:48:15 -0400

    add

--- baseline/run.sh
+++ baseline/run.sh
@@ -12,8 +12,8 @@ done
 
 echo "Process $TARGET_PID has ended. Running your script..."
 
-#python sgl_baseline.py --seed 456 --start 100 --end 500 --model Qwen/QwQ-32B
-python sgl_baseline.py --seed 789 --start 156 --end 500 --model Qwen/QwQ-32B
+python sgl_baseline.py --seed 456 --start 100 --end 500 --model Qwen/QwQ-32B
+#python sgl_baseline.py --seed 789 --start 156 --end 500 --model Qwen/QwQ-32B
 
 #python baseline.py --seed 2015 --start 0 --end 30 --model Qwen/QwQ-32B
 #python baseline.py --seed 2015 --start 0 --end 30 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
