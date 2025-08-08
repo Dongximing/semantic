97d176837f0d6d8327a916a87ac432eb094e1c24
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-08-08 09:56:30 -0400

    add

--- baseline/run.sh
+++ baseline/run.sh
@@ -12,7 +12,7 @@ done
 
 echo "Process $TARGET_PID has ended. Running your script..."
 
-python sgl_baseline.py --seed 456 --start 100 --end 500 --model Qwen/QwQ-32B
+python sgl_baseline.py --seed 456 --start 212 --end 500 --model Qwen/QwQ-32B
 #python sgl_baseline.py --seed 789 --start 156 --end 500 --model Qwen/QwQ-32B
 
 #python baseline.py --seed 2015 --start 0 --end 30 --model Qwen/QwQ-32B
