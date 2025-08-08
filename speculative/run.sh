549b7dfa854555756b505e532bdce1a7af911d8c
Ximing Dong <ximing@XimingdeMacBook-Pro.local>
2025-08-08 10:41:29 -0400

    add

--- speculative/run.sh
+++ speculative/run.sh
@@ -18,7 +18,7 @@ echo "Process $TARGET_PID has ended. Running your script..."
 #python speculative_hf_decoding.py --seed 2023   --dataset math-500 --start_dataset 100 --end_dataset 500 --data_dir /data/semantic/speculative/speed_up_spec_result_math-500_deepseek_r132_deepseek1.5seed_
 #python speculative_sglang_decoding.py --seed 2013   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python speculative_sglang_decoding.py --seed 2025   --dataset math-500 --start_dataset 101 --end_dataset 500
-python speculative_sglang_decoding.py --seed 30   --dataset math-500 --start_dataset 211 --end_dataset 500
+python speculative_sglang_decoding.py --seed 301   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python speculative_sglang_decoding.py --seed 78   --dataset math-500 --start_dataset 118 --end_dataset 500
 #python speculative_sglang_decoding.py --seed 99   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python slg/generate_samples_sglang.py
