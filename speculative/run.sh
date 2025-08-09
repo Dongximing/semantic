5ef860974a6d586b61508cc319b103432df52266
Ximing Dong <ximing@ae:f0:5b:4c:74:36.home>
2025-08-09 15:21:00 -0400

    change new nn nework

--- speculative/run.sh
+++ speculative/run.sh
@@ -22,7 +22,7 @@ echo "Process $TARGET_PID has ended. Running your script..."
 #python speculative_sglang_decoding.py --seed 301   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python speculative_sglang_decoding.py --seed 7811   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python speculative_sglang_decoding.py --seed 78   --dataset math-500 --start_dataset 118 --end_dataset 500
-python speculative_sglang_decoding.py --seed 999   --dataset math-500 --start_dataset 100 --end_dataset 500
+python speculative_sglang_decoding.py --seed 127   --dataset math-500 --start_dataset 100 --end_dataset 500
 #python slg/generate_samples_sglang.py
 
 #python speculative_hf_decoding.py --seed 2004   --dataset aime --start_dataset 0 --end_dataset 30 --data_dir /data/semantic/speculative/speed_up_spec_result_aime-30_deepseek_r132_deepseek1.5seed_
