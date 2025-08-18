50b778ad2eeee3c695937b90882a679dfcf06ac5
Dongximing <dongxx1104@gmail.com>
2025-08-18 16:13:29 -0400

    add result

--- baseline/run_wax.sh
+++ baseline/run_wax.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python offline_sgl_baseline.py --seed 123 --end 40
-python offline_sgl_baseline.py --seed 456 --end 40
-python offline_sgl_baseline.py --seed 789 --end 40
+# python offline_sgl_baseline.py --seed 123 --end 40
+# python offline_sgl_baseline.py --seed 456 --end 40
+python offline_sgl_baseline.py --seed 9101112 --end 40
